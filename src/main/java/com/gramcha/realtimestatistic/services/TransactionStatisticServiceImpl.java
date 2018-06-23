fe5a84fb9a093cee90a1ebb8b298dad6b4256413
Ganeshkumar Ramachandran <haiganeshkumar@gmail.com>
2018-06-24 01:16:39 +0530

    core service implementation

--- src/main/java/com/gramcha/realtimestatistic/services/TransactionStatisticServiceImpl.java
+++ src/main/java/com/gramcha/realtimestatistic/services/TransactionStatisticServiceImpl.java
@@ -4,28 +4,56 @@
  * 
  */
 package com.gramcha.realtimestatistic.services;
+
+import org.assertj.core.util.VisibleForTesting;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.gramcha.realtimestatistic.Utilities.TimeStampHelper;
 import com.gramcha.realtimestatistic.models.StatisticsDto;
 
 @Service
 public class TransactionStatisticServiceImpl implements TransactionStatisticService {
 
-	/* (non-Javadoc)
-	 * @see com.gramcha.realtimestatistic.services.TransactionStatisticService#addTransaction(double, long)
+	@Autowired
+	private CircularStatisticsLotBuffer lotBuffer;
+	@Autowired
+	StatisticsAggregator aggregator;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.gramcha.realtimestatistic.services.TransactionStatisticService#
+	 * addTransaction(double, long)
 	 */
 	@Override
 	public void addTransaction(double amount, long timestamp) {
 		// TODO Auto-generated method stub
-		
+		StatisticsLot result = lotBuffer.add(amount, timestamp);
+		System.out.println("Added Result = "+result);
 	}
 
-	/* (non-Javadoc)
-	 * @see com.gramcha.realtimestatistic.services.TransactionStatisticService#getStatisticsForInterval()
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.gramcha.realtimestatistic.services.TransactionStatisticService#
+	 * getStatisticsForInterval()
 	 */
 	@Override
 	public StatisticsDto getStatisticsForInterval() {
 		// TODO Auto-generated method stub
-		return null;
+		StatisticsDto result = new StatisticsDto();
+		long currentTimeStamp = TimeStampHelper.getCurrentTimeinMS();
+		for (int i = 0; i < lotBuffer.size(); i++) {
+			StatisticsLot lot = lotBuffer.get(i);
+			if(TimeStampHelper.isInTimeInterval(lot.getTimeStamp(), currentTimeStamp, 60)) {//move 60 seconds to prop file.
+				result = aggregator.aggregateTwoLots(result, lot.getStatistics());
+			}
+		}
+		return result;
 	}
-
+	@VisibleForTesting
+	void resetBuffer() {
+		lotBuffer.resetBufferWithdefault();
+	}
 }
