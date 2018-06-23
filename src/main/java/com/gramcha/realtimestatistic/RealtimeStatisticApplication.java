8b5a90622effd636f83f19adac0c24833ddbb6fe
Ganeshkumar Ramachandran <haiganeshkumar@gmail.com>
2018-06-23 13:26:48 +0530

    setting up spring-boot boilerplate

---
+++ src/main/java/com/gramcha/realtimestatistic/RealtimeStatisticApplication.java
@@ -0,0 +1,12 @@
+package com.gramcha.realtimestatistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeStatisticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeStatisticApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gramcha/realtimestatistic/RealtimeStatisticApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gramcha.realtimestatistic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RealtimeStatisticApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
